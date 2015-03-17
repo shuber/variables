c5234ebc122b4002d3a46b0e9403486e1e6518e8
Sean Huber <github@shuber.io>
2015-03-16 18:03:56 -0700

    Don't retest logic in CoreExt::Object

--- spec/variables/core_ext/object_spec.rb
+++ spec/variables/core_ext/object_spec.rb
@@ -14,36 +14,23 @@ RSpec.describe Variables::CoreExt::Object do
   end
 
   describe '#instance_variable_fetch' do
-    it 'should return the value if it is defined' do
-      subject.instance_variable_set('@test', :example)
-      value = subject.instance_variable_fetch(:test)
+    it 'should delegate #fetch to the named instance variable' do
+      var = double('instance variable')
+      block = proc { }
+      expect(subject).to receive(:instance_variable).with(:test).and_return(var)
+      expect(var).to receive(:fetch).with(:a, :b, &block).and_return(:example)
+      value = subject.instance_variable_fetch(:test, :a, :b, &block)
       expect(value).to eq(:example)
-    end
-
-    it 'should raise UndefinedVariable if the value is not defined' do
-      action = proc { subject.instance_variable_fetch(:test) }
-      expect(action).to raise_error(Variables::UndefinedVariable)
     end
   end
 
   describe '#instance_variable_replace' do
-    it 'should replace a variable and return the old value' do
-      expect(subject.instance_variable_replace(:test, :value)).to be_nil
-      expect(subject.instance_variable_get('@test')).to eq(:value)
-    end
-
-    it 'should replace a variable for the duration of a block' do
-      expect(subject.instance_variable_get('@undefined')).to be_nil
-
-      subject.instance_variable_replace(:undefined, :value) do
-        expect(subject.instance_variable_get('@undefined')).to eq(:value)
-      end
-
-      expect(subject.instance_variable_get('@undefined')).to be_nil
-    end
-
-    it 'should return the last expression of a block' do
-      value = subject.instance_variable_replace(:test, :value) { :example }
+    it 'should delegate #replace to the named instance variable' do
+      var = double('instance variable')
+      block = proc { }
+      expect(subject).to receive(:instance_variable).with(:test).and_return(var)
+      expect(var).to receive(:replace).with(:a, :b, &block).and_return(:example)
+      value = subject.instance_variable_replace(:test, :a, :b, &block)
       expect(value).to eq(:example)
     end
   end
